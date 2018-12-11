5b4d5321be6a3963b886bb6347b81b708c0100fa
tshemsedinov <timur.shemsedinov@gmail.com>
2018-12-11 13:48:46 +0200

    Add async methods and async static examples

--- JavaScript/2-sync.js
+++ JavaScript/2-sync.js
@@ -15,7 +15,30 @@ const avg = (a, b) => {
   return s / 2;
 };
 
-console.log('inc(5) = ' + inc(5));
-console.log('sum(1, 3) = ' + sum(1, 3));
-console.log('max(8, 6) = ' + max(8, 6));
-console.log('avg(8, 6) = ' + avg(8, 6));
+const obj = {
+  name: 'Marcus Aurelius',
+  split(sep = ' ') {
+    return this.name.split(sep);
+  }
+};
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  static of(name) {
+    return new Person(name);
+  }
+  split(sep = ' ') {
+    return this.name.split(sep);
+  }
+}
+
+const person = Person.of('Marcus Aurelius');
+
+console.log('inc(5) =', inc(5));
+console.log('sum(1, 3) =', sum(1, 3));
+console.log('max(8, 6) =', max(8, 6));
+console.log('avg(8, 6) =', avg(8, 6));
+console.log('obj.split() =', obj.split());
+console.log('person.split() =', person.split());
--- JavaScript/3-async.js
+++ JavaScript/3-async.js
@@ -15,11 +15,34 @@ const avg = async (a, b) => {
   return s / 2;
 };
 
+const obj = {
+  name: 'Marcus Aurelius',
+  async split(sep = ' ') {
+    return this.name.split(sep);
+  }
+};
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  static async of(name) {
+    return await new Person(name);
+  }
+  async split(sep = ' ') {
+    return this.name.split(sep);
+  }
+}
+
+const person = new Person('Marcus Aurelius');
+
 (async () => {
 
-  console.log('inc(5) =', await inc(5));
-  console.log('sum(1, 3) =', await sum(1, 3));
-  console.log('max(8, 6) =', await max(8, 6));
-  console.log('avg(8, 6) =', await avg(8, 6));
+  console.log('await inc(5) =', await inc(5));
+  console.log('await sum(1, 3) =', await sum(1, 3));
+  console.log('await max(8, 6) =', await max(8, 6));
+  console.log('await avg(8, 6) =', await avg(8, 6));
+  console.log('await obj.split() =', await obj.split());
+  console.log('await person.split() =', await person.split());
 
 })();
