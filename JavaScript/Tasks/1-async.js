e32af673ebb86d68c2832690eaa209afbbe1b48c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-03-13 21:16:06 +0200

    Add tasks

---
+++ JavaScript/Tasks/1-async.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Task: rewrite `total` from callbacks contract to async/await
+// Hint: do not forget to catch errors with try/catch block
+
+const total = (items, callback) => {
+  let result = 0;
+  for (const item of items) {
+    if (item.price < 0) {
+      callback(new Error('Negative price is not allowed'));
+      return;
+    }
+    result += item.price;
+  }
+  callback(null, result);
+};
+
+const electronics = [
+  { name: 'Laptop', price: 1500 },
+  { name: 'Keyboard', price: 100 },
+  { name: 'HDMI cable', price: 10 },
+];
+
+total(electronics, (error, money) => {
+  if (error) console.error({ error });
+  else console.log({ money });
+});
